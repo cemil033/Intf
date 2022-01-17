7e7d9f8a98e9aff4b41940c24ef852ac09a76885
cemil <cemiw033@gmail.com>
2022-01-17 19:48:31 +0400

    Interface

---
+++ ConsoleApp5/Class1.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5
+{
+    
+    abstract public class Storage
+    {
+        public string? Data { get; set; }
+        public string? MediaName { get; set ; }
+        public string? Model { get; set; }
+        public double Size { get ; set; }
+        public int filescount { get; set; }
+        public double Speed { get; set; }
+        virtual public double Freememory()
+        {
+            return Size-(filescount*780);
+        }
+        virtual public void Copy(Storage a)
+        {
+            a.Data = Data;
+        }
+        virtual public void ShowDeviceInfo()
+        {
+            Console.WriteLine($"Device name: {MediaName}");
+            Console.WriteLine($"Model name: {Model}");
+            Console.WriteLine($"Size: {Size}");
+        }
+        virtual public void Check()
+        {
+            Console.WriteLine($"{780000/Size}: device lazimdir");
+            Console.WriteLine($"{780000/Speed}: saniye vaxt lazimdir");
+        }
+    }
+    public class Flash : Storage
+    {
+        
+        public override void Copy(Storage a)
+        {
+            base.Copy(a);
+        }
+
+        public override double Freememory()
+        {
+            return base.Freememory();
+        }
+
+        public override void ShowDeviceInfo()
+        {
+            base.ShowDeviceInfo();
+        }
+
+        public override void Check()
+        {
+            base.Check();
+        }
+    }
+    public class DVD : Storage
+    {
+        public override void Copy(Storage a)
+        {
+            base.Copy(a);
+        }
+
+        public override double Freememory()
+        {
+            return base.Freememory();
+        }
+
+        public override void ShowDeviceInfo()
+        {
+            base.ShowDeviceInfo();
+        }
+
+        public override void Check()
+        {
+            base.Check();
+        }
+    }
+
+    public class HDD : Storage
+    {
+        public override void Copy(Storage a)
+        {
+            base.Copy(a);
+        }
+
+        public override double Freememory()
+        {
+            return base.Freememory();
+        }
+
+        public override void ShowDeviceInfo()
+        {
+            base.ShowDeviceInfo();
+        }
+
+        public override void Check()
+        {
+            base.Check();
+        }
+    }
+
+}
---
+++ ConsoleApp5/Program.cs
@@ -0,0 +1,47 @@
+﻿using ConsoleApp5;
+Flash flash = new()
+{
+    MediaName = "SAMSUNG",
+    Model = "S-12",
+    Size = 2340,
+    Speed = 100,
+    filescount = 1000,
+    Data="salam"
+};
+DVD dVD = new() { 
+    MediaName = "Disc1x",
+    Model= "Disc1x",
+    Size = 3900,
+    Speed = 78,
+    filescount = 1000
+
+};
+HDD dHDD = new() 
+{
+    MediaName = "Seagate",
+    Model = "gt-390",
+    Size = 390000,
+    Speed = 78,
+    filescount = 1000,
+    Data="sagol"
+};
+DVD VD = new()
+{
+    MediaName = "Disc2x",
+    Model = "Disc2x",
+    Size = 7800,
+    Speed = 78,
+    filescount = 1000,
+    Data ="Hakuna matata"
+};
+
+flash.Copy(dVD);
+Console.WriteLine(dVD.Data); ;
+Console.WriteLine("\n");
+
+VD.Copy(dHDD);
+Console.WriteLine(dHDD.Data);
+Console.WriteLine("\n");
+
+
+
